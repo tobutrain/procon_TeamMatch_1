0a86004a88071109c6b29e22b084a48888fd92ce
tobutrain_10000 <lamy.yukihana@tobutrain.net>
2022-12-16 20:56:33 +0900

    csv reading

--- TeamMatch.py
+++ TeamMatch.py
@@ -5,13 +5,13 @@ from gensim.models import word2vec
 #Word2Vecモデルのパスを指定
 w2vPath = "./jawiki.all_vectors.100d.txt"
 #類似度を測定するデータの指定
-fileDirectory = "./testFile.csv"
+csvDirectory = "./testFile.csv"
 
 #以下変数定義
 csv_header = ['ID','Word1','Word2','Word3']
 dictIDKeyword = {}
 num = 0
-w2vModel = word2vec.KeyedVectors.load_word2vec_format(w2vPath,binary=False,encoding='utf-8')
+#w2vModel = word2vec.KeyedVectors.load_word2vec_format(w2vPath,binary=False,encoding='utf-8')
 
 #仕様: cosWord 
 #IN: 単語1,単語2(文字列) OUT:cos類似度(0<=n<=1) (実数型)
@@ -20,11 +20,11 @@ def cosWord(word1,word2):
     vec2 = w2vModel[word2]
     return np.dot(vec1,vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
 
+#csvDirectoryに指定したcsvファイルをdictIDKeyword(辞書型)に落とし込む
+# dictIDKeyword = {key -> 0-n(グループ数), value -> {key -> csv_header : value -> csv内の値(チームID,収集した単語など)}}
+#ex) dictIDKeyword[0]['ID'] = 101 , dictIDKeyword[0]['Word1']='かっつー' 
 
-#csv開いてごちゃごちゃしようとした跡地 消してもらっても使ってもらっても
-
-#with open(fileDirectory) as dictFile :
-#    for row in csv.DictReader(dictFile,csv_header):
-#        print(row)
-#        dictIDKeyword[num] = row
-#        num += 1
+with open(csvDirectory) as dictFile :
+    for row in csv.DictReader(dictFile,csv_header):
+        dictIDKeyword[num] = row
+        num += 1
